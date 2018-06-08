af4765307a878ec1112865be8e0d525cdb0fcd98
Mepa Yohann <yohann.mepa@fixstudio.com>
2018-06-08 18:25:28 +0200

    stuck with 3d load

---
+++ content.py
@@ -0,0 +1,43 @@
+from flask import Markup
+from datetime import datetime
+
+years= ''
+months= ''
+
+about = Markup("""
+        <p>
+            my name is Yohann, 
+            <span id="small">I use to do some 2d/3d animation </span>
+            now recicled in <span id="different">Python Development</span> for %s years and %smonth now,
+        </p>
+        <p>
+            I use to some Vr, Unity/Unreal stuff, University Lecturer
+            Suzzane Award wining, Special slection to Annecy, etc..
+        </p> 
+        <p>
+            I am currently working For Fixstudio as
+            Python Developen / R&D Engenier
+
+            you still can =>cantact me <=
+        </p>""") % (years, months)
+
+rand_name = ['Mamadou', 'La Grenouille', 'Mon Chelly']
+
+
+### inspiration
+## https://kentatoshikura.com/
+## http://findmatthew.com/
+## https://pierre.io/
+## http://www.michaelvillar.com/
+## https://optimo.com/
+## http://slyillustrations.com/
+## https://www.eliwilliamson.com/
+## http://www.drama.xxx/
+
+## lib 
+## http://animejs.com/
+## http://mojs.io/
+## http://three.js/
+
+# # host
+#  https://www.pythonanywhere.com/forums/topic/1496/
---
+++ static/app.js
@@ -0,0 +1,62 @@
+// Three.js
+
+var container = document.getElementById('three');
+// document.write(container);
+var mouse = new THREE.Vector2();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(1, 1, 2);
+var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshPhongMaterial( { 				
+    color: Math.random() * 0xff00000 - 0xff00000,
+    shading: THREE.FlatShading } 
+);
+
+
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+light = new THREE.PointLight(0xffffff, 1, 4000);
+light.position.set(5, 0, 0);
+light_two = new THREE.PointLight(0xffffff, 1, 4000);
+light_two.position.set(-1, 8, 8);
+lightAmbient = new THREE.AmbientLight(0x404040);
+scene.add(light, light_two, lightAmbient);
+
+camera.position.z = 5;
+renderer.setSize( window.innerWidth,  window.innerHeight );
+
+document.body.appendChild( container );
+document.addEventListener('mousemove', onMouseMove, false);
+container.appendChild( renderer.domElement );
+function animate() 
+    {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    }
+    function onMouseMove(event) {
+
+        mouseX = event.clientX - window.innerWidth / 2;
+        mouseY = event.clientY - window.innerHeight / 2;
+        camera.position.x += (mouseX - camera.position.x) * 0.0003;
+        camera.position.y += (mouseY - camera.position.y) * 0.0003;
+        if (camera.position.x < -2) {
+            camera.position.x = -2
+        }
+        if (camera.position.x > 2 ) {
+            camera.position.x = 2
+        }
+        if (camera.position.y < -1) {
+            camera.position.y = -1
+        }
+        if (camera.position.y > 1 ) {
+            camera.position.y = 1
+        }
+        
+        console.log('camera x',camera.position.x);
+        console.log('camera y', camera.position.y);
+        camera.lookAt(scene.position);
+    };
+    
+animate();
