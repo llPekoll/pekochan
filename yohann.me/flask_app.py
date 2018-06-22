cfcc78005313595e295c7d744ebba4f381458860
Mepa Yohann <yohann.mepa@fixstudio.com>
2018-06-22 11:14:26 +0200

    updated yohann.me

--- yohann.me/flask_app.py
+++ yohann.me/flask_app.py
@@ -1,14 +1,21 @@
 
 from flask import Flask, render_template
 from random import randint
-import content as ct
+# import content as ct
 app = Flask(__name__)
 
+
+
+from datetime import datetime
+from collections import namedtuple
+
+now = datetime.now()
+date = now.year, now.strftime('%B'), now.strftime('%a'), str(now.today().day)
+
+
 @app.route('/')
-@app.route('/<name>')
 def index(name=None):
-    rand_nb = randint(0, len(ct.rand_name)-1)
-    return render_template('index.html'), 200
+    return render_template('index.html',date=date,), 200
 
 # if __name__ == '__main__':
 #     app.run()
