cbcf6c73b3a4bcee81c4d86763f5e0acb0510cfd
Yohann Mepa <yohann.mepa@fixstudio.com>
2019-04-04 23:59:21 +0200

    loop animation on the crown

--- static/app.js
+++ static/app.js
@@ -3,7 +3,6 @@ var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('backgr
 renderer.autoClear = false;
 renderer.setSize( window.innerWidth,  window.innerHeight );
 
-document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 document.addEventListener( 'resize', onWindowResize, false );
 var scene = new THREE.Scene();
@@ -12,7 +11,6 @@ var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHe
 var camera2 = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
 camera2.position.set(1, 1, 800);
 camera.position.set(1, 1, 800);
-
 
 
 // postprocessing
@@ -30,10 +28,6 @@ effect.renderToScreen = true;
 composer.addPass( effect );
 
 
-// var pass1 = new THREE.UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5/2, 0.4, 0.87 );
-// composer.addPass( pass1 );
-// pass1.renderToScreen = true;
-
 var pass2 = new THREE.GlitchPass( 100 );
 composer.addPass( pass2 );
 pass2.renderToScreen = true;
@@ -41,7 +35,6 @@ pass2.renderToScreen = true;
 
 var mouse = { x: 0, y: 0 }, INTERSECTED;
 var raycaster = new THREE.Raycaster();
-var projector = new THREE.Projector();
 render();
 
 
@@ -73,59 +66,7 @@ function render() {
     renderer.clearDepth();
     composer.render()
     renderer.clearDepth();
-    renderer.render( scene2, camera2 );
-    update()
-
-}
-
-function update()
-{
-    var raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera(mouse, camera2);
-	var intersects = raycaster.intersectObjects( scene2.children );
-	if ( intersects.length > 0 )
-	{
-		if ( intersects[ 0 ].object != INTERSECTED ) 
-		{
-		    // restore previous intersection object (if it exists) to its original color
-			if ( INTERSECTED ) 
-				INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-			// store reference to closest object as current intersection object
-			INTERSECTED = intersects[ 0 ].object;
-			// store color of closest object (for later restoration)
-			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
-			// set a new color for closest object
-			INTERSECTED.material.color.setHex( 0xffff00 );
-		}
-	} 
-	else // there are no intersections
-	{
-		// restore previous intersection object (if it exists) to its original color
-		if ( INTERSECTED ) 
-			INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-		// remove previous intersection object reference
-		//     by setting current intersection object to "nothing"
-		INTERSECTED = null;
-	}
-
-}
-
-function onDocumentMouseDown( event ) 
-{
-	
-	console.log("Click.");
-
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-    var raycaster2 = new THREE.Raycaster();
-    raycaster2.setFromCamera(mouse, camera2);
-	var intersects2 = raycaster2.intersectObjects( scene2.children );
-	
-	if ( intersects2.length > 0 )
-	{
-		console.log("Hit @ " + toString( intersects2[0] ) );
-		intersects2[ 0 ].object.material.color.set( 0xff0000 );
-	}
+	renderer.render( scene2, camera2 );
+	TWEEN.update();
 
 }
--- static/mesh.js
+++ static/mesh.js
@@ -31,21 +31,21 @@ loader.load(peko,
     }
 );
 
-loader.load(crown,
-    function ( crown ) {
-        crown.material = new THREE.MeshPhongMaterial({color: 0xdddddd});
-        crown.scale.set(30,30,30);
-        crown.position.set(5,260,70);
-        crown.position.y -= 40;
-        scene.add( crown );
-    },
-    function ( xhr ) {
-        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    },
-    function ( err ) {
-        console.error( 'An error happened' );
-    }
-);
+// loader.load(crown,
+//     function ( crown ) {
+//         crown.material = new THREE.MeshPhongMaterial({color: 0xdddddd});
+//         crown.scale.set(30,30,30);
+//         crown.position.set(5,260,70);
+//         crown.position.y -= 40;
+//         scene.add( crown );
+//     },
+//     function ( xhr ) {
+//         console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+//     },
+//     function ( err ) {
+//         console.error( 'An error happened' );
+//     }
+// );
 
 loader.load(wings,
     function ( wings ) {
@@ -63,3 +63,73 @@ loader.load(wings,
     }
 );
 
+// Instantiate a loader
+var loader = new THREE.GLTFLoader();
+
+// // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+// THREE.DRACOLoader.setDecoderPath( '/examples/js/libs/draco' );
+// loader.setDRACOLoader( new THREE.DRACOLoader() );
+	
+// // Optional: Pre-fetch Draco WASM/JS module, to save time while parsing.
+// THREE.DRACOLoader.getDecoderModule();
+
+// Load a glTF resource
+
+loader.load(
+	// resource URL
+	crownGltf,
+	// called when the resource is loaded
+	function ( crown ) {
+        crown.scene.children[0].name = "crown"
+        scene.add( crown.scene );
+        crown.scene.children[0].scale.set(25,25,25);
+        crown.material = new THREE.MeshPhongMaterial({color: 0xdddddd});
+        crown.animations; // Array<THREE.AnimationClip>
+		crown.scene; // THREE.Scene
+		crown.scenes; // Array<THREE.Scene>
+        crown.cameras; // Array<THREE.Camera>
+		crown.asset; // Object
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+
+// var mmd = scene.getObjectByName( "crown", true );
+// console.log(mmd);
+
+// for (let elt of scene.children) 
+// {
+//     console.log(elt);
+// }
+
+
+
+window.onload = function () { 
+    var jose = scene.getObjectByName( "crown" );
+    console.log( jose );
+    var position = { x : 0, y: 0 };
+    var target = { x : 0, y: 10 };
+    var tween = new TWEEN.Tween(position).to(target, 2000).yoyo( true ).repeat( Infinity ).easing( TWEEN.Easing.Cubic.InOut );
+    
+    tween.onUpdate(function(){
+        jose.position.x = position.x;
+        jose.position.y = position.y;
+    });
+
+    // tween.easing(TWEEN.Easing.elasticOut)
+    // tween.
+    tween.start();
+
+
+}
