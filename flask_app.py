d788efb122c951d70cd612c49553ded1e950a0ba
Yohann Mepa <yohann.mepa@fixstudio.com>
2019-04-07 14:31:21 +0200

    fix bug prenivous commit not working

--- flask_app.py
+++ flask_app.py
@@ -1,45 +1,16 @@
 import os
 from flask import Flask, render_template, Markup
 from random import randint
-from datetime import datetime
-from collections import namedtuple
-
-
+import content as ct
 app = Flask(__name__)
-
-now = datetime.now()
-
-def get_python_time():
-    
-    Day = namedtuple('Day','year month')
-    pday = Day(2016,4)
-    years = now.year - pday.year
-    months = now.month - pday.month
-
-    total_year = int((years*12 + months)/12)
-    total_month = int((years*12 + months)%12)
-    return total_year, total_month
-
-
-csss = [['highlightGreen.css','Gcv.png','Ggit.png','Gin.png','green'],
-        ['highlightPink.css','Pcv.png','Pgit.png','Pin.png','pink'],
-        ['highlightOrange.css','Ocv.png','Ogit.png','Oin.png','orange']]
-
-rand_name = ['Mamadou',
-            'La Grenouille',
-            'Mon Chelly',
-            'Thomas',
-            'Theary',
-            'Sisi',
-            'Juju de fruit']
 
 @app.route('/')
 def index():
-    rand_name = randint(0, len(rand_name)-1)
-    rand_css = randint(0, len(csss)-1)
-    years, months = get_python_time()
-    return render_template('index.html', rand=rand_name[rand_name],
-                            css=csss[rand_css],
+    rand_name = randint(0, len(ct.rand_name)-1)
+    rand_css = randint(0, len(ct.csss)-1)
+    years, months = ct.get_python_time()
+    return render_template('index.html', rand=ct.rand_name[rand_name],
+                             css=ct.csss[rand_css],
                             years=years,
                             months=months), 200
 
